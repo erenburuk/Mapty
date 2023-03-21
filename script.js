aa0ec47250ff79bbababd3f9594ea17e788f3864
erenburuk <erenburukk@gmail.com>
2023-03-22 02:18:07 +0300

    Fix: fix bug

--- script.js
+++ script.js
@@ -267,12 +267,12 @@ class App {
       html += `
           <div class="workout__details">
             <span class="workout__icon">⚡️</span>
-            <span class="workout__value">${workout.speed}</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
             <span class="workout__unit">km/h</span>
           </div>
           <div class="workout__details">
             <span class="workout__icon">⛰</span>
-            <span class="workout__value">${workout.elevation}</span>
+            <span class="workout__value">${workout.elevationGain}</span>
             <span class="workout__unit">m</span>
           </div>
         </li>
